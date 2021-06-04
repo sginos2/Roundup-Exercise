34c325d10c699ba5add356f8d0b5958b4aa6d46b
Shanna Ginos <Shanna@Dustins-MBP.directcom.com>
2021-06-04 13:21:54 -0600

    added writing to file functionality

--- roundup.js
+++ roundup.js
@@ -1,13 +1,49 @@
 const csv = require('csv-parser');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;  
 const fs = require('fs');
-let total = 0;
+
+let tuitionData;
+
+const csvWriter = createCsvWriter({  
+  path: 'resultFile.csv',
+  header: [
+    {id: 'id', title: 'id'},
+    {id: 'firstName', title: 'firstName'},
+    {id: 'lastName', title: 'lastName'},
+    {id: 'credit', title: 'Total Credit'},
+    {id: 'tuition', title: 'tuition'},
+  ]
+});
+
+let id, firstName, lastName, credit, tuition;
+
 fs.createReadStream('studentRegistration.csv')
   .pipe(csv())
   .on('data', (row) => {
       if (row.id === '1234') {
-          total += Number(row.credit);
+          id = row.id;
+          firstName = row.firstName;
+          lastName = row.lastName;
+          credit += Number(row.credit);
+
       }
   })
   .on('end', () => {
-    console.log(`Total credit hours: ${total}`);
+    let status;
+    if (credit >= 12) {
+      status = 'Full Time';
+    } else if (credit < 12) {
+      status = 'Part Time';
+    }
+    let report = {
+      id: 1234,
+      firstName: firstName,
+      lastName: lastName,
+      credit: Number(credit),
+      tuition: 350
+    };
+    csvWriter.writeRecords([report]).then(() => {
+      console.log('..DONE');
+    });
+    console.log(`Complete`);
   });
